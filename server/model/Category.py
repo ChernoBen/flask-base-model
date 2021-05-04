aae9ce7e56cac446481e2affb4650724cbe30538
benjamim <bneto@computacao.cua.ufmt.br>
2021-05-04 15:29:07 -0300

    setting Category

--- server/model/Category.py
+++ server/model/Category.py
@@ -6,3 +6,7 @@ from config import app_active,app_config
 config = app_config[app_active]
 db = SQLAlchemy(config.APP)
 
+class Category(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(20),unique=True,nullable=False)
+    description = db.Column(db.Text(),nullable=False)
