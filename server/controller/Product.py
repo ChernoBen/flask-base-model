d2161a54ec3825738d088f03c828d39d547fbdfe
benjamim <bneto@computacao.cua.ufmt.br>
2021-05-05 11:12:38 -0300

    product model put a new product

---
+++ server/controller/Product.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from model.Product import Product
+
+class ProductController():
+    def __init__(self):
+        self.product_model = Product()
+
+    def save_product(self,obj):
+        self.product_model.name = obj['name']
+        self.product_model.description = obj['description']
+        self.product_model.qtd = obj['qtd']
+        self.product_model.price = obj['price']
+        self.product_model.date_created = datetime.now()
+        self.product_model.category = obj['category']
+        return self.product_model.save()
--- server/model/Product.py
+++ server/model/Product.py
@@ -16,7 +16,7 @@ class Product(db.Model):
     qtd = db.Column(db.Integer,nullable=True,default=0)
     image = db.Column(db.Text(),nullable=True)
     price = db.Column(db.Numeric(10,2),nullable=False)
-    data_created = db.Column(db.DateTime(6),default=db.func.current_timestamp(),nullable=False)
+    date_created = db.Column(db.DateTime(6),default=db.func.current_timestamp(),nullable=False)
     last_update = db.Column(db.DateTime(6),onupdate=db.func.current_timestamp(),nullable=False)
     category = db.Column(db.Integer,db.ForeignKey(Category.id),nullable=False)
     #user = relationship(User)
